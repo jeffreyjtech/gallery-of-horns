b00e439787806b686f096226f03ecf36172df67e
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-15 15:43:16 -0800

    Added data import and beastDataset props to Main

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
+import beastsDataset from './data.json';
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Header />
-        <Main />
+        <Main beastsDataset={beastsDataset}/>
         <Footer />
       </>
     );
