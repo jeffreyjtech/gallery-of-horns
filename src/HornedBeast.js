924e1f7a4fdd74fc41abf3757296e3ea4dbf7964
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-15 15:44:00 -0800

    Added key prop, renamed existing prop references

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -4,11 +4,15 @@ import './HornedBeast.css';
 class HornedBeast extends React.Component {
   render() {
     return(
-      <article>
-        <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title}></img>
+      <article key={this.props.idx}>
+        <h2>{this.props.beastData.title}</h2>
+        <img 
+          src={this.props.beastData.image_url}
+          alt={this.props.beastData.description} 
+          title={this.props.beastData.title}
+        />
         <p>
-          {this.props.description}
+          {this.props.beastData.description}
         </p>
       </article>
     );
