d288fbcc397952f0a04f0d244c6d51d0a364fa54
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-15 15:55:14 -0800

    Added constructor, onClick handler, and dynamic votes

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -2,6 +2,19 @@ import React from "react";
 import './HornedBeast.css';
 
 class HornedBeast extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      votes: 0
+    }
+  }
+
+  handleVote = () =>  {
+    this.setState({
+      votes: this.state.votes + 1
+    })
+  }
+
   render() {
     return(
       <article key={this.props.idx}>
@@ -10,10 +23,12 @@ class HornedBeast extends React.Component {
           src={this.props.beastData.image_url}
           alt={this.props.beastData.description} 
           title={this.props.beastData.title}
+          onClick={this.handleVote}
         />
         <p>
           {this.props.beastData.description}
         </p>
+        <span>❤ {this.state.votes}</span>
       </article>
     );
   }
