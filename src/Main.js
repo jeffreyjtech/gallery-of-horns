f3383a90a53ffd15e3a36fde03581365855b17fc
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-14 20:00:34 -0800

    Added props to 2nd component

--- src/Main.js
+++ src/Main.js
@@ -10,7 +10,11 @@ class Main extends React.Component {
           imageUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
           description="A unicorn and a narwhal nuzzling their horns"
         />
-        <HornedBeast />
+        <HornedBeast 
+          title="Rhino Family"
+          imageUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
+          description="Parent rhino with two babies"
+        />
       </main>
     );
   }
