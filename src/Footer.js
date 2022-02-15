379adf63f7f84413bcb1ce45767d23534f932b88
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-14 19:44:00 -0800

    Created Footer component

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return(
+      <footer>
+        Jeffrey Jenkins
+      </footer>
+    );
+  }
+}
+
+export default Footer;
