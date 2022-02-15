7eedea3f9ccc0d2e432f14cee74e43e41446138a
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-15 15:44:40 -0800

    Added .map() method to create a comp for each beast

--- src/Main.js
+++ src/Main.js
@@ -4,18 +4,12 @@ import './Main.css';
 
 class Main extends React.Component {
   render() {
+    let beastComps = this.props.beastsDataset.map(
+      (element, idx) => <HornedBeast beastData={element} key={idx} />
+    );
     return(
       <main>
-        <HornedBeast 
-          title="UniWhal" 
-          imageUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
-          description="A unicorn and a narwhal nuzzling their horns"
-        />
-        <HornedBeast 
-          title="Rhino Family"
-          imageUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
-          description="Parent rhino with two babies"
-        />
+        {beastComps}
       </main>
     );
   }
