7a3eb83860f97120f73444fe1539f0349d642fb9
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-15 16:22:03 -0800

    Imported essential bootstrap css file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import beastsDataset from './data.json';
 
 class App extends React.Component {
