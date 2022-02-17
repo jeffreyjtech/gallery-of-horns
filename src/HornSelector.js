0f96e917c936545b1ab56dcb56f905bfcd6164ec
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-02-17 15:46:31 -0800

    Added horn selector form

---
+++ src/HornSelector.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Form from 'react-bootstrap/Form';
+
+class HornSelector extends React.Component {
+  handleSelect = (event) => {
+    let parsedHornQty = Number.parseInt(event.target.value)
+    this.props.handleHorns(parsedHornQty);
+  };
+
+  render() {
+    return (
+      <Form className="ms-4 w-25">
+        <Form.Group>
+          <Form.Label htmlFor="formSelect">Horn select</Form.Label>
+          <Form.Select id="formSelect" onChange={this.handleSelect}>
+            <option>{null}</option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>100</option>
+          </Form.Select>
+        </Form.Group>
+      </Form>
+    );
+  }
+}
+
+export default HornSelector;
